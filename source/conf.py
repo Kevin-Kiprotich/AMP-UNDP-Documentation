0541d15045104214287463c7c52e659fdebb4337
Kevin-Kiprotich <kevinkiprotich0089@gmail.com>
2026-04-02 15:03:11 +0300

    Project title changed

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'AMP'
+project = 'Africa Minigrids Program'
 copyright = '2026, LocateIT Ke'
 author = 'LocateIT Ltd'
 release = '1.0'
