14e7b00be935564a5711d2d6284708cc44c25195
Kevin-Kiprotich <kevinkiprotich0089@gmail.com>
2026-04-10 00:35:22 +0300

    Pydata theme added

--- source/conf.py
+++ source/conf.py
@@ -24,6 +24,25 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 html_css_files=['css/custom.css']
+
+
+html_theme_options = {
+	'canonical_url': '',
+    'analytics_id': '',  #  Provided by Google in your dashboard
+    #'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    #'vcs_pageview_mode': '',
+    'style_nav_header_background': 'black',
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 5,
+    'includehidden': False,
+    'titles_only': False
+	}
